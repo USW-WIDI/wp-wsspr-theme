3668a695775b2226d5c475182d75f11cc98b6f0e
kali <sharktallica@gmail.com>
2023-09-24 16:19:31 +0100

    Add Simon requested changes and support for mozilla pdf.js

--- library/bones.php
+++ library/bones.php
@@ -142,6 +142,7 @@ function bones_scripts_and_styles() {
 
 		//adding scripts file in the footer
 		wp_register_script( 'bones-js', get_stylesheet_directory_uri() . '/library/js/scripts.js', array( 'jquery' ), '', true );
+		wp_register_script( 'wsspr-js', get_stylesheet_directory_uri() . '/library/js/wsspr.js', array( 'jquery' ), '', true );
 
 		// enqueue styles and scripts
 		wp_enqueue_script( 'bones-modernizr' );
@@ -158,6 +159,7 @@ function bones_scripts_and_styles() {
 		*/
 		wp_enqueue_script( 'jquery' );
 		wp_enqueue_script( 'bones-js' );
+		wp_enqueue_script( 'wsspr-js' );
 
 	}
 }
---
+++ library/js/wsspr.js
@@ -0,0 +1,56 @@
+/******************************************************************
+
+Script: Support for WSSPR theme
+
+******************************************************************/
+
+var iframe = document.getElementsByClassName("pdfjs-iframe")[0];
+
+function waitForEle(selector){
+    return new Promise(resolve => {
+        if (iframe.contentWindow.document.querySelector(selector)) {
+            return resolve(iframe.contentWindow.document.querySelector(selector));
+        }
+
+        const observer = new MutationObserver(mutations => {
+            if (iframe.contentWindow.document.querySelector(selector)) {
+                observer.disconnect();
+                resolve(iframe.contentWindow.document.querySelector(selector));
+            }
+        });
+
+        observer.observe(iframe.contentWindow.document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
+
+iframe.onload = function()
+{
+    waitForEle('.toolbar').then((ele) =>
+    {
+        ele.style.display = "none";
+
+        waitForEle('#viewerContainer').then((ele2) =>
+        {
+            ele2.style.top = 0;
+
+            waitForEle('.pdfViewer').then((ele3) =>
+            {
+                ele3.style.backgroundColor = "white";
+
+                waitForEle('.pdfViewer .page').then((ele4) =>
+                {
+                    ele4.style.border = 0;
+                    ele4.style.padding = 0;
+                    
+                    waitForEle('#viewerContainer').then((ele5) =>
+                    {
+                        ele5.style.backgroundColor = "white";
+                    });
+                });
+            });
+        });
+    });
+};
