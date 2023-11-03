24fbdc6a814fdf49ba7336537b14af4ef71d01dc
kali <sharktallica@gmail.com>
2023-11-03 01:45:37 +0000

    Remove unprofessional comment from original author

--- page.php
+++ page.php
@@ -28,19 +28,12 @@
 
 								<section class="entry-content cf" itemprop="articleBody">
 									<?php
-										// the content (pretty self explanatory huh)
+										// the content
 										the_content();
 
 										/*
 										 * Link Pages is used in case you have posts that are set to break into
 										 * multiple pages. You can remove this if you don't plan on doing that.
-										 *
-										 * Also, breaking content up into multiple pages is a horrible experience,
-										 * so don't do it. While there are SOME edge cases where this is useful, it's
-										 * mostly used for people to get more ad views. It's up to you but if you want
-										 * to do it, you're wrong and I hate you. (Ok, I still love you but just not as much)
-										 *
-										 * http://gizmodo.com/5841121/google-wants-to-help-you-avoid-stupid-annoying-multiple-page-articles
 										 *
 										*/
 										wp_link_pages( array(
